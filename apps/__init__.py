2f689e55bab77b34f20f3858c4ea7cd9868e1944
zhaopeng <zhaopeng1@kingsoft.com>
2017-12-23 17:51:51 +0800

    1.将数据库引擎更换为mysql 2.注册2个应用，并将应用都统一放到apps目录下

---
+++ apps/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2017/12/23 17:27
+# @Author  : HankZhao
+# @File    : __init__.py.py
+# @Describe:
--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+sys.path.insert(0, os.path.join(BASE_DIR,"apps"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.message',
+    'apps.users'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,14 @@ WSGI_APPLICATION = 'djangoproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': "djangotest",
+        'USER':"root",
+        'PASSWORD':"root",
+        'HOST':"207.148.67.119",
+        'PORT':"3306",
     }
 }
 
